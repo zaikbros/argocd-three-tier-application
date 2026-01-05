39ed3573c53ea2032d653d4f2b18619ae78c224d
zaikbros <125757460+zaikbros@users.noreply.github.com>
2026-01-05 13:36:50 +0530

    Revert "Dev"

--- frontend/src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: '/', // Relative path, handled by Ingress
-});
-
-export default api;
--- frontend/src/types/Employee.ts
+++
@@ -1,8 +0,0 @@
-export interface Employee {
-    empno?: number;
-    empname: string;
-    job: string;
-    sal: number;
-    deptno: number;
-    vflag?: string;
-}
